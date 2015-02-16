f0b9025f80783d1da0cc289810c149390559c957
Tripp Roberts <tripp@tripp-roberts.com>
2015-02-16 12:35:54 -0500

    Adds phone number, first/last name, role, and organization_id to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150206172459) do
+ActiveRecord::Schema.define(version: 20150216173234) do
 
   create_table "users", force: true do |t|
     t.datetime "created_at"
@@ -26,6 +26,11 @@ ActiveRecord::Schema.define(version: 20150206172459) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "phone_number"
+    t.integer  "organization_id"
+    t.string   "role"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
