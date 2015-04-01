ab946caff0ba489896d5ca0fb288d6936e55dbf2
Tripp Roberts <tripp@tripp-roberts.com>
2015-04-01 02:37:08 -0400

    Adds routes for announcements

--- config/routes.rb
+++ config/routes.rb
@@ -4,15 +4,17 @@ Rails.application.routes.draw do
       get "/logout" => "devise/sessions#destroy"
   end
   root 'users#dashboard'
-  get "/dashboard" => "users#dashboard"
+  get "/dashboard" => "users#dashboard", as: "user_dashboard"
   get "/users" => "organizations#users", as: "organization_users"
   get "/announcements" => "organizations#announcements", as: "organization_announcements"
 
   #announcement resources
   patch "/announcements/:id" => "announcements#update"
   get "/announcements/new" => "announcements#new", as: "new_announcement"
+  post "/announcements" => "announcements#create"
   get "/announcements/:id/edit" => "announcements#edit", as: "edit_announcement"
   delete "/announcements/:id" => "announcements#destroy", as: "destroy_announcement"
+  get "/announcements/:id" => "announcements#show", as: "announcement"
 
   #user resources
   patch "/users/:id" => "users#update"
